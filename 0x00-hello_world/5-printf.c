bb871180d7fbceb802af3f5d2f442f9f84f4136b
Sertsedengle Shewandagn <53481194+Sertsev@users.noreply.github.com>
2021-02-04 23:25:24 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	char My_message[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", My_message);
+	return (0);
 }
