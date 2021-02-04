2e659aabf5f7ab5a0fe2f97c2f2c56cbb586db81
Sertsedengle Shewandagn <53481194+Sertsev@users.noreply.github.com>
2021-02-04 23:32:29 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
-#include <stdio.io>
+#include <stdio.h>
+#include <unistd.h>
 
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main(void) {
-  write(1, "Hello World!\n", 13); 
-  return(1);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
