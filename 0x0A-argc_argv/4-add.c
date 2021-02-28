44c34cef44d23e9d9348cb95e5609d735f52502f
sersev <sertseshewa@gmail.com>
2021-02-28 19:15:13 +0000

    arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - a main afunction to add positive numbers,
+ *@argc: counts arguments
+ *@argv: build a vector array of arguments
+ *Return: returns success 0.
+ */
+
+int main(int argc, char **argv)
+{
+int a = 0, i, sum = 0;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+a = 0;
+a += atoi(argv[i]);
+
+if (a == 0)
+{
+printf("Error\n");
+break;
+}
+else
+{
+sum += atoi(argv[i]);
+}
+}
+if (a != 0)
+printf("%d\n", sum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
+}
