275df4bb568f2d0b9bfebb89c09331c1cedff247
sertsev <sertseshewa@gmail.com>
2021-07-01 11:31:02 +0300

    following betty style and modifying compiler errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,26 @@
 
 char *_strcat(char *dest, char *src)
 {
-    char *dest1 = dest;
-    while (*dest++)
-    {
-        if (*dest == '\0'){
-            *dest = *src;
-            *src++;
-        }
-        
-    }
-    
-    while (*src++){
-        *dest = *src;
-        *src++;
-        *dest++;
-    }
-   
-    return dest1;
+	char c, *dest1 = dest;
+
+	while (*dest++)
+	{
+		if (*dest == '\0')
+		{
+			*dest = *src;
+			c = *src++;
+		}
+	}
+
+	while (*src++)
+	{
+		*dest = *src;
+		c = *src++;
+		c = *dest++;
+	}
+
+	c = '\0';
+	*dest = c;
+
+	return (dest1);
 }
