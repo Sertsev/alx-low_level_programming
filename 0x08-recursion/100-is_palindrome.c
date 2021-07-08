0b052c72eefc0453dcf750bee0f142887f1bd112
sertsev <sertseshewa@gmail.com>
2021-07-08 15:54:54 +0300

    deleting duplication and fixing betty issues of the palindrome file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,25 @@
 #include "holberton.h"
+
+/**
+ * is_palindrome - a function to find palindrome strings
+ * @s: a string pointer
+ *
+ * Return: returns 1 or 0
+ */
+
 int is_palindrome(char *s)
 {
-int i = 0, j = 0, m;
+int i = 0, j = 0;
+char cp[1024];
 
 if (*s == '\0')
 {
-return 1;
+return (1);
 }
 while (s[i] != '\0')
 {
 i++;
 }
-
-char cp[i];
 
 while (s[j] != '\0')
 {
@@ -20,9 +27,9 @@ cp[j] = s[--i];
 
 if (cp[j] != s[j])
 {
-return 0;
+return (0);
 }
 j++;
 }
-return 1;
+return (1);
 }
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int is_palindrome(char *s);
 void _putchar(char c);
 void _puts_recursion(char *s);
 
