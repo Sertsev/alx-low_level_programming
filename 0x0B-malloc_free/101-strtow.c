d57cd7ec74335816e8a583d23065b7981eef60db
sertsev <sertseshewa@gmail.com>
2021-07-14 23:17:39 +0300

    strtow function modification

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+
+/**
+ * wcre - finds a word
+ *
+ * @words: an array of strings
+ * @str: string pointer
+ * @i: integer value
+ * @k: array index
+ * Return: returns an integer
+ */
+
+int wcre(char **words, char *str, int i, int k)
+{
+int j = 0;
+
+words[k] = malloc(128);
+
+for (; str[i] != ' '; i++)
+{
+words[k][j] = str[i];
+j++;
+}
+words[k][j] = '\0';
+
+return (i);
+}
+
+/**
+ * strtow - finds words from the long string
+ *
+ * @str: a string pointer
+ * Return: a pointer to pointer of a string
+ */
+
+char **strtow(char *str)
+{
+char **words;
+int i = 0, k = 0;
+
+words = malloc(256);
+
+if (str == NULL || str == "")
+return (NULL);
+
+while (str[i] != '\0')
+{
+if (str[i] == ' ')
+{
+i++;
+continue;
+}
+else
+{
+i = wcre(words, str, i, k);
+k++;
+}
+}
+return (words);
+}
+
