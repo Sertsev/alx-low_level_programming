2b9d80cf977bfa9a5083611e633328617ed1f1e8
Sertsedengle Shewandagn <sertseshewa@gmail.com>
2021-07-14 23:28:38 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,21 +11,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-unsigned int i =  0;
-char *array = malloc(size);
+char *array = NULL;
+unsigned int i;
 
 if (size == 0)
-{
 return (NULL);
-}
-else
+if (size != 0)
+{
+array = (char *)malloc(size * sizeof(char));
+if (array != NULL)
 {
 for (i = 0; i < size; i++)
-{
 array[i] = c;
 }
-return (array);
 }
-
-return (0);
+return (array);
 }
