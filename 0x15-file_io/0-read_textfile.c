e6fd81db1b8945e5b019c7ea92b072e0a8c1a1a2
sertsev <sertseshewa@gmail.com>
2021-08-13 17:43:47 +0300

    updating main.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * read_textfile - reads textfile and prints to stdout
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * closes_file - Closes a file.
